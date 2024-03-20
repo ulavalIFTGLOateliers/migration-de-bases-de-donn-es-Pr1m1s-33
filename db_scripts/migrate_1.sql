ccb41ff3f7020e03e7bf299ea5487e2d893d0417
Christopher Averty <162889783+Pr1m1s-33@users.noreply.github.com>
2024-03-20 21:30:02 +0100

    Update migrate_1.sql

--- db_scripts/migrate_1.sql
+++ db_scripts/migrate_1.sql
@@ -1,1 +1,17 @@
 ALTER TABLE singer RENAME TO musician;
+
+ALTER TABLE musician ADD COLUMN email VARCHAR(100);
+
+ALTER TABLE musician MODIFY COLUMN age DECIMAL(5, 2);
+
+CREATE TABLE IF NOT EXISTS concert (
+    concertName VARCHAR(50),
+    musicianName VARCHAR(50),
+    location VARCHAR(100),
+    concertDate DATE,
+    PRIMARY KEY (concertName),
+    CONSTRAINT FK_C_musicianName FOREIGN KEY (musicianName) REFERENCES musician(singerName)
+);
+
+INSERT INTO musician (singerName, firstName, lastName, age, email) VALUES ("Nova", "Nora", "Vance", 29, "nora@example.com");
+INSERT INTO concert VALUES ("Nova Live", "Nova", "The Grand Hall", "2024-05-30");
